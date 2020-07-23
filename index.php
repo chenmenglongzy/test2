c157403c808c6959dac3449a72aa2b0165decd96
chenmenglongzy <longzhongyi@songshuai.com>
2020-07-23 16:09:29 +0800

    修改描述

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
 <?php
-echo md5("128").PHP_EOL;die;
-//user_id  8880100895635125
-//6860100895635588
-//school_id  2556
-class Index{
-    public function __construct()
-    {
-        echo 'hello word'.'\n';
-    }
-
-    public function test(){
-
-        echo 'test_METHOD';
-    }
-
-    public function test2(){
-
-        return 'hello word';
-      echo  '222';
-      echo '333';
-
-
-    }
-}
-
-$test = new Index();
-$test->test();
+echo 'hello word';
 
 
 
